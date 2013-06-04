b77c55291cdb57830cd6530f527ef6719b1e8b61
Ari Rizzitano <arizzitano@gmail.com>
2013-06-03 23:24:06 -0400

    reorder files & set up urlconf

--- petbot.js
+++ petbot.js
@@ -2,12 +2,15 @@ var arduino = require('duino');
 var app = require('http').createServer(handler);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
-var __static = './public';
+var __dirname = './public';
 var pinMap = {
 	'forward': '02',
 	'back': '03',
 	'left': '04',
 	'right': '05'
+};
+var urlconf = {
+	'/': '/index.html'
 };
 
 var board = new arduino.Board({
@@ -17,16 +20,15 @@ var board = new arduino.Board({
 app.listen(8080);
 
 function handler (req, res) {
-  fs.readFile(__static + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
+	var urlToServe = (urlconf[req.url] != null) ? urlconf[req.url] : req.url;
+	fs.readFile(__dirname + urlToServe, function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading '+urlToServe);
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
 }
 
 function handleSignal(direction, active) {
@@ -36,9 +38,8 @@ function handleSignal(direction, active) {
 }
 
 io.sockets.on('connection', function (socket) {
-    console.log('working');
-  socket.on('direction', function (data) {
-    handleSignal(data.name, data.active);
-    //board.digitalWrite('09', board.HIGH);
-  });
+	console.log('working');
+	socket.on('direction', function (data) {
+    	handleSignal(data.name, data.active);
+	});
 });
