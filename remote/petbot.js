ff3daadd13ad8d35915d39fa184e09464d7e258e
Ari Rizzitano <arizzitano@gmail.com>
2013-06-11 00:26:02 -0400

    cleanup & add some explanation to page

--- remote/petbot.js
+++ remote/petbot.js
@@ -1,39 +1,18 @@
 var express = require('express');
-//var http = require('http');
 var app = express();
-//var server = http.createServer(app);
 var socket = require('socket.io');
-
-var fs = require('fs');
-
-var urlconf = {
-	'/': '/index.html'
-};
 
 app.configure(function () {
 	app.use(express.static(__dirname + '/../public'));
 	
 	var username = process.env.AUTH_USERNAME;
-	var password = process.env.PASSWORD;
+	var password = process.env.AUTH_PASSWORD;
 	if (username && password) {
 		app.use(express.basicAuth(username, password));
 	}
 });
 
 var server = app.listen(process.env.PORT || 5000);
-/*
-function handler (req, res) {
-	var urlToServe = (urlconf[req.url] != null) ? urlconf[req.url] : req.url;
-	fs.readFile(__dirname + urlToServe, function (err, data) {
-		if (err) {
-			res.writeHead(500);
-			return res.end('Error loading '+urlToServe);
-		}
-		res.writeHead(200);
-		res.end(data);
-	});
-}
-*/
 
 var io = socket.listen(server);
 
