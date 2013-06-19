3cafd0fbef1f95847e058b317500a7db23478df5
Dan Riti <dmriti@gmail.com>
2013-06-19 00:27:11 -0400

    Rename constants for clarity.

--- common/config.js
+++ common/config.js
@@ -1,10 +1,10 @@
 // misc settings
-var DEBUG  = true;
+var DEBUG = true;
 
 // host settings
-var HOST_REMOTE = 'http://petbot.herokuapp.com';
-var HOST_LOCAL  = 'http://localhost:5000';
-var HOST        = DEBUG ? HOST_LOCAL : HOST_REMOTE;
+var HOST_PRODUCTION  = 'http://petbot.herokuapp.com';
+var HOST_DEVELOPMENT = 'http://localhost:5000';
+var HOST             = DEBUG ? HOST_DEVELOPMENT : HOST_PRODUCTION;
 
 module.exports.DEBUG = DEBUG;
 module.exports.HOST  = HOST;
